887de12bbcc70c3f5d7864b8b109a6f0d04c019e
TuanAnh <nguyentuan6730@gmail.com>
2024-03-30 14:19:39 +0100

    initial version

---
+++ src/main/java/spring/io/state/managment/StateManagmentApplication.java
@@ -0,0 +1,13 @@
+package spring.io.state.managment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StateManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StateManagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/io/state/managment/StateManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.io.state.managment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StateManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
