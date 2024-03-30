8af5228773ec1a6f0aedc5547f166ee1a4685490
TuanAnh <nguyentuan6730@gmail.com>
2024-03-30 14:40:46 +0100

    create Server entity

---
+++ src/main/java/spring/io/state/managment/model/Server.java
@@ -0,0 +1,33 @@
+package spring.io.state.managment.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Server {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(unique = true)
+    @NotEmpty(message = "IP Address is required")
+    private String ipAddress;
+
+    private String name;
+
+    private String memory;
+
+    private String type;
+
+    private String imageUrl;
+
+    private Status status;
+}
